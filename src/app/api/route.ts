77dbec4b6db57423beafe652dbbd37a3f426eb03
kanasuan hansuddhisuntorn <90129274+kanasuanH@users.noreply.github.com>
2024-11-28 20:55:25 +0700

    try v4

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -15,28 +15,49 @@ export async function POST(req: Request) {
     });
   
     const page = await browser.newPage();
-
-    const htmlContent = `<html><body><h1>Your PDF Content</h1></body></html>`
   
-    await page.setContent(htmlContent, { waitUntil: 'load' });
-
-    const pdfBuffer = await page.pdf({
-      format: 'A4',
-      printBackground: true,
-    });
+    const url = `https://news.ycombinator.com`;
+    await page.goto(url);
+  
+    const [image, title] = await Promise.all([page.screenshot(), page.title()]);
   
     await page.close();
     await browser.close();
 
-      const response = new NextResponse(pdfBuffer, {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/pdf',
-          'Content-Disposition': 'attachment; filename=output.pdf',
-        },
-      });
+    return NextResponse.json({ error: 'It run' }, { status: 200 });
+    
 
-      return response;
+    // const install = require(`puppeteer/internal/node/install.js`).downloadBrowser;
+    // await install();
+  
+    // const browser = await puppeteer.launch({
+    //   args: ["--use-gl=angle", "--use-angle=swiftshader", "--single-process", "--no-sandbox"],
+    //   headless: true,
+    // });
+  
+    // const page = await browser.newPage();
+
+    // const htmlContent = `<html><body><h1>Your PDF Content</h1></body></html>`
+  
+    // await page.setContent(htmlContent, { waitUntil: 'load' });
+
+    // const pdfBuffer = await page.pdf({
+    //   format: 'A4',
+    //   printBackground: true,
+    // });
+  
+    // await page.close();
+    // await browser.close();
+
+    //   const response = new NextResponse(pdfBuffer, {
+    //     status: 200,
+    //     headers: {
+    //       'Content-Type': 'application/pdf',
+    //       'Content-Disposition': 'attachment; filename=output.pdf',
+    //     },
+    //   });
+
+    //   return response;
 
       
   } catch (error) {
